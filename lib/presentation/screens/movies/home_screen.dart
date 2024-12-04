42bfafa32b25a7ec41fc4b8a6c3b27d020194f4f
Juan Carlos <juankelpocho@gmail.com>
2024-12-04 00:57:42 -0500

    primera face terminada

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:cinemapedia/config/constants/enviroment.dart';
+import 'package:cinemapedia/repositories/providers/movies/Movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
 class HomeScreen extends StatelessWidget {
@@ -10,9 +12,42 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body: Center(
-        child: Text(Enviroment.movieDbkey),
-      ),
+      body: _HomeView()
     );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView({
+    super.key,
+  });
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+
+    if (nowPlayingMovies.length == 0) return Center(child: CircularProgressIndicator());
+
+    return ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: (context, index) {
+        final movie = nowPlayingMovies[index];
+        return ListTile(
+          title: Text('${index + 1}- ${movie.title}'),
+        );
+      },
+      );
   }
 }
